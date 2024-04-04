217ab870b1e472404bcaa9ee052882a4a2688574
AakashKumar-Code <aakashkumar27022001@gmail.com>
2024-04-04 11:05:04 +0530

    Backend Partial Complete & Frontend Initialized
    Create & Read API's implemented in Spring Boot & MySQL. Frontend initialized using ReactJs.

---
+++ SpringBoot-BackEnd-CRUD-app/src/main/java/com/AakashKumarCode/SpringBootBackEndCRUDapp/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.AakashKumarCode.SpringBootBackEndCRUDapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.AakashKumarCode.SpringBootBackEndCRUDapp.model.User;
+import com.AakashKumarCode.SpringBootBackEndCRUDapp.repository.UserRepository;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@PostMapping("/user")
+	User newUser(@RequestBody User newUser) {
+		return userRepo.save(newUser);
+	}
+	
+	@GetMapping("/users")
+	List<User> getAllUsers(){
+		return userRepo.findAll();
+	}
+
+}
